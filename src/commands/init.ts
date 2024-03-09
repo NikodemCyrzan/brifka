f8960b05fd0b3d593f7033c85f099fd842760e89
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2024-03-10 00:59:30 +0100

    feat: added init command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,6 +1,24 @@
 import ArgsParser from "../argsParser";
+import { createDirectory, writeFile } from "../files";
+import path from "node:path";
 
 const init = (argsParser: ArgsParser) => {
+    const repo = "./.brifka";
+
+    const join = (...paths: string[]) => path.join(repo, ...paths);
+
+    writeFile(join("mem/commits"));
+    writeFile(join("mem/traced"));
+
+    createDirectory(join("rep"))
+
+    writeFile("brifka.config.json", JSON.stringify({
+        server: "",
+        port: 21,
+        login: "",
+        password: ""
+    }));
+    writeFile(".brignore", "brifka.config.json");
 }
 
 export default init;
