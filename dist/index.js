e879ff7692762faff2da5914516f28ed0adb2f22
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2024-03-09 21:05:48 +0100

    init

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+"use strict";console.log("inited succesfully");
---
+++ rollup.config.mjs
@@ -0,0 +1,12 @@
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+    input: "./src/index.ts",
+    output: {
+        file: "dist/index.js",
+        format: "cjs",
+        plugins: [terser()],
+    },
+    plugins: [typescript({ tsconfig: "tsconfig.json" })],
+};
