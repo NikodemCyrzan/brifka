2b5c4862d40beda7bebce7e14f1bc5314978f79e
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2024-03-13 16:38:09 +0100

    fix: fixed example env variable

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,12 +1,11 @@
 import chalk from "chalk";
 import ArgsParser from "../argsParser";
 import { Config } from "../config";
-import fs from "node:fs/promises";
 import { createDirectory, writeFile } from "../files";
 import logText from "../console";
 import { BRIGNORE, COMMITS, CONFIG, HEAD, REPOSITORY, TRACKED } from "../paths";
 
-const init = async (argsParser: ArgsParser) => {
+const init = (argsParser: ArgsParser) => {
 	writeFile(COMMITS);
 	writeFile(TRACKED);
 	writeFile(HEAD);
@@ -15,7 +14,7 @@ const init = async (argsParser: ArgsParser) => {
 
 	const defaultConfig: Config = {
 		env: {
-			"BRIFKA_EXAMPLE": JSON.parse(await fs.readFile("package.json", "utf8"))?.version ?? "error"
+			"BRIFKA_EXAMPLE": "Hello world!"
 		},
 		ftp: {
 			host: "localhost",
